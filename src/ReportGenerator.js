08fc97db9dfde066467f95b9cd33845d5402f74b
Lucas Flor Vilela <lucasvilelapessoal@gmail.com>
2026-05-15 18:40:49 -0300

    Commit Correcao de Erros Iniciais

--- src/ReportGenerator.js
+++ src/ReportGenerator.js
@@ -36,8 +36,9 @@ export class ReportGenerator {
           report += `${item.id},${item.name},${item.value},${user.name}\n`;
           total += item.value;
         } else if (reportType === 'HTML') {
-          const style = item.priority ? 'style="font-weight:bold;"' : '';
-          report += `<tr ${style}><td>${item.id}</td><td>${item.name}</td><td>${item.value}</td></tr>\n`;
+          // AJUSTE AQUI: O espaço foi movido para dentro da string condicional
+          const style = item.priority ? ' style="font-weight:bold;"' : '';
+          report += `<tr${style}><td>${item.id}</td><td>${item.name}</td><td>${item.value}</td></tr>\n`;
           total += item.value;
         }
       } else if (user.role === 'USER') {
